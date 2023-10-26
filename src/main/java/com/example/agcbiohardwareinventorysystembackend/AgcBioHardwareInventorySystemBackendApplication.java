c4b653a8e635ac8ee8493362b842a3976e184dab
Mubíbic <90600712+mubibic@users.noreply.github.com>
2023-10-26 14:05:13 +0200

    Created spring project

--- src/main/java/com/example/agcbiohardwareinventorysystembackend/AgcBioHardwareInventorySystemBackendApplication.java
+++ src/main/java/com/example/agcbiohardwareinventorysystembackend/AgcBioHardwareInventorySystemBackendApplication.java
@@ -9,5 +9,5 @@ public class AgcBioHardwareInventorySystemBackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AgcBioHardwareInventorySystemBackendApplication.class, args);
 	}
-
+//hello test
 }
