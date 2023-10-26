f3b8c37febdbacf9e72eec0e7eb9d22a8664dde1
Mubíbic <90600712+mubibic@users.noreply.github.com>
2023-10-26 13:57:38 +0200

    Created spring project

---
+++ src/main/java/com/example/agcbiohardwareinventorysystembackend/AgcBioHardwareInventorySystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.agcbiohardwareinventorysystembackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgcBioHardwareInventorySystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgcBioHardwareInventorySystemBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/agcbiohardwareinventorysystembackend/AgcBioHardwareInventorySystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.agcbiohardwareinventorysystembackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgcBioHardwareInventorySystemBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
