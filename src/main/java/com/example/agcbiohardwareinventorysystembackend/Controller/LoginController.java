62c798d55b1b8bd1d84964690582e76ad26fe7ea
BalazsGitHub <balzs.dombovai@gmail.com>
2023-12-06 20:07:50 +0100

    Changed logincontroller and commented out springboot security in POM

--- src/main/java/com/example/agcbiohardwareinventorysystembackend/Controller/LoginController.java
+++ src/main/java/com/example/agcbiohardwareinventorysystembackend/Controller/LoginController.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:63342")
+@CrossOrigin(origins = "*")
 @RequestMapping("/login")
 public class LoginController {
 
